07e3f8039773780e7e61b6722f5c0eb10fdd8ed7
Shi Hao Wang <st890409@hotmail.com>
2018-09-07 22:31:04 +0800

    驗證 png 圖片格式方法

---
+++ FileValidatorTest/PngFormatTest.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using Xunit;
+
+namespace FileValidatorTest
+{
+
+    public class PngFormatTest : FileFormatTestProvider
+    {
+
+        [Fact]
+        public void RealFileTest()
+        {
+            string _fullPath;
+            string _fileName;
+            byte[] _content;
+            
+            bool _validResult;
+
+            _fullPath = @"../../../Files/github-txstudio-screenshot.png";
+            
+            _fileName = Path.GetFileName(_fullPath);
+            _content = File.ReadAllBytes(_fullPath);
+
+            _validResult = this.Validator.ValidFormat(_fileName, _content);
+            
+            Assert.True(_validResult);
+        }
+    }
+}
