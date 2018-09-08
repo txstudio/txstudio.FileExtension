fe065cdd858a325d87543e45577da8d0160bc491
Shi Hao Wang <st890409@hotmail.com>
2018-09-08 15:52:29 +0800

    修改取得 header 的測試程式碼

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,38 +10,25 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            //var _config = new FileHeaderConfig();
-            //_config.Name = "jpeg";
-            //_config.Extensions = new string[] { "jpg", "jpeg"};
+            IEnumerable<string> _paths;
 
-            //var _bytes = new List<byte[]>();
-            //_bytes.Add(new byte[] { 1, 233, 51 });
-            //_bytes.Add(new byte[] { 203, 5, 79 });
+            _paths = new string[]
+            {
+                "../../../filename1.gif",
+                "../../../filename2.gif",
+            };
 
-            //_config.PrefixBytes = _bytes;
-
-            //var _json = JsonConvert.SerializeObject(_config, Formatting.Indented);
-
-            string _filePath = @"../../../github-txstudio-screenshot.gif";
             byte[] _content;
 
-            _content = File.ReadAllBytes(_filePath);
-            var _take = _content.Take(50);
+            foreach (string _path in _paths)
+            {
+                _content = File.ReadAllBytes(_path);
+                var _take = _content.Take(50);
 
-            Console.WriteLine(string.Join(",", _take));
-            Console.WriteLine();
-
-
-
-            _filePath = @"../../../replacement.gif";
-
-            _content = File.ReadAllBytes(_filePath);
-            _take = _content.Take(50);
-
-            Console.WriteLine(string.Join(",", _take));
-            Console.WriteLine();
-
-
+                Console.WriteLine(string.Join(",", _take));
+                Console.WriteLine();
+            }
+            
             Console.WriteLine("press any to exit");
             Console.ReadKey();
         }
