5e56aa18f9edc321d52b4e243bbd9d3ae2766603
Shi Hao Wang <st890409@hotmail.com>
2018-09-08 15:49:01 +0800

    加入 jpg 格式圖片驗證測試

---
+++ FileValidatorTest/JpgFormatTest.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using Xunit;
+
+namespace FileValidatorTest
+{
+
+    public class JpgFormatTest : FileFormatTestProvider
+    {
+
+        [Fact]
+        public void RealFileTest()
+        {
+            string _fullPath;
+            string _fileName;
+            byte[] _content;
+            
+            bool _validResult;
+
+            _fullPath = this.GetFullPath(@"github-txstudio-screenshot.jpg");
+            
+            _fileName = Path.GetFileName(_fullPath);
+            _content = File.ReadAllBytes(_fullPath);
+
+            _validResult = this.Validator.ValidFormat(_fileName, _content);
+            
+            Assert.True(_validResult);
+        }
+
+        [Fact]
+        public void WrongFileTest()
+        {
+            string _fullPath;
+            string _fileName;
+            byte[] _content;
+
+            bool _validResult;
+
+            _fullPath = this.GetFullPath(@"github-txstudio-screenshot.jpg");
+            _fileName = Path.GetFileName(_fullPath);
+
+            _fullPath = this.GetFullPath(@"github-txstudio-screenshot.gif");
+            _content = File.ReadAllBytes(_fullPath);
+
+            _validResult = this.Validator.ValidFormat(_fileName, _content);
+
+            Assert.False(_validResult);
+        }
+    }
+}
